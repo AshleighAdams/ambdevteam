063e177b4009001cb7df7c0c1447a196f4ef8a0f
Kate Adams <self@kateadams.eu>
2010-02-01 14:07:53 +0000

    Repositioned some of the drawing functions to move most bugs and add features.
    git-svn-id: http://ambdevteam.googlecode.com/svn/trunk@81 1e2648ae-0935-11df-a26f-8b716316a5bc

--- AmBFightToSurvive/gamemode/cl_recource_systems.lua
+++ AmBFightToSurvive/gamemode/cl_recource_systems.lua
@@ -6,13 +6,32 @@ concommand.Add( "dev_showresp", function(pl, cmd, args)
 	LocalPlayer():ChatPrint( GetResP( pl:Team() ) )
 end)
 
-function GM:HUDPaint()
+function DrawBox( x, y, text, isgood )
+	local colbg,colfg = Color(100,50,0),Color(255,100,0)
+	if isgood then
+		colbg,colfg = Color(0,50,0,100), Color(50,255,50,255)
+	else
+		colbg,colfg = Color(50,0,0,100), Color(255,50,50,255)
+	end
+	
+	-- Make it stay on screen always
+	BoxSize = Vector( ScrW()/16, ScrH()/16, 0 )
+	local Scrw = ScrW() - BoxSize.x
+	local Scrh = ScrH() - BoxSize.y
+	
+	x = math.Clamp( x, 25, Scrw )
+	y = math.Clamp( y, 25, Scrh )
+	
+	draw.WordBox( 8, x, y, text,"Default", colbg, colfg )
+end
+
+function GM:HUDPaintBackground()
 	local pl = LocalPlayer()
 	local count = 0
 	-- resource drops
 	for k,v in pairs( ents.FindByClass("resource_drop") ) do
 		local pos = v:GetPos():ToScreen()
-		draw.WordBox( 8, pos.x, pos.y, "Capture","Default", Color(50,0,0,100), Color(255,50,50,255) )
+		DrawBox(pos.x, pos.y, "Capture", false)
 		count = k
 	end
 	-- crystal carriers
@@ -21,65 +40,54 @@ function GM:HUDPaint()
 		local BoxCol = Color(50,0,0,100)
 		local TextCol = Color(255,50,50,255)
 		local Text = "Capture"
-		
 		local owner = v.Owner
+		pl.HasCrystal = false
 		
 		if ValidEntity(owner) && (pl:Team() == owner:Team()) then
-			BoxCol = Color(0,50,0,100)
-			TextCol = Color(50,255,50,255)
-			Text = "Escort"
 			for l,r in pairs( ents.FindByClass("refinery") ) do
 				if r.Team == pl:Team() then
-					refinery = r
+						refinery = r
+						if owner == pl then                            ---- were holding the refinery
+							if ValidEntity( refinery ) then
+								pl.HasCrystal = true
+								pos = refinery:GetPos():ToScreen()
+								Text = "Refine"
+								DrawBox( pos.x, pos.y, Text, true )
+							end
+						else
+							if ValidEntity( refinery ) then
+							pos = refinery:GetPos():ToScreen()
+							Text = "Escort To"
+							DrawBox( pos.x, pos.y, Text, true )
+						end
+						if !pl.HasCrystal then
+							pos = owner:GetPos():ToScreen()
+							Text = "Escort"
+							DrawBox( pos.x, pos.y, Text, true )
+						end
+					end
 				end
 			end
 			
-			if owner == pl then
-				if ValidEntity( refinery ) then
-					pos = refinery:GetPos():ToScreen()
-					BoxCol = Color(0,50,0,100)
-					TextCol = Color(50,255,50,255)
-					Text = "Refine"
-					draw.WordBox( 8, pos.x, pos.y, Text,"Default", BoxCol, TextCol )
-				end
-				
-			else
-				if ValidEntity( refinery ) then
-					pos = refinery:GetPos():ToScreen()
-					BoxCol = Color(0,50,0,100)
-					TextCol = Color(50,255,50,255)
-					Text = "Refine"
-					draw.WordBox( 8, pos.x, pos.y, Text,"Default", BoxCol, TextCol )
-				end
-				pos = owner:GetPos():ToScreen()
-				BoxCol = Color(0,50,0,100)
-				TextCol = Color(50,255,50,255)
-				Text = "Escort"
-				draw.WordBox( 8, pos.x, pos.y, Text,"Default", BoxCol, TextCol )
-			end
 		elseif ValidEntity( owner ) then
 			pos = owner:GetPos():ToScreen()
-			BoxCol = Color(0,50,0,100)
-			TextCol = Color(50,255,50,255)
 			Text = "Kill"
-			draw.WordBox( 8, pos.x, pos.y, Text,"Default", BoxCol, TextCol )
+			DrawBox( pos.x, pos.y, Text, false )
 		end
-		draw.WordBox( 8, pos.x, pos.y, Text,"Default", BoxCol, TextCol )
+		DrawBox( pos.x, pos.y, Text, false )
 		count = count + k
 	end
 	if count == 0 then
 		for k,r in pairs( ents.FindByClass("refinery") ) do
 			local pos = r:GetPos():ToScreen()
-			local BoxCol = Color(50,0,0,100)
-			local TextCol = Color(255,50,50,255)
 			local Text = "Attack"
+			local good = false
 			if r.Team == pl:Team() then
 				pos = r:GetPos():ToScreen()
-				BoxCol = Color(0,50,0,100)
-				TextCol = Color(50,255,50,255)
+				good = true
 				Text = "Defend"
 			end
-			draw.WordBox( 8, pos.x, pos.y, Text,"Default", BoxCol, TextCol )
+			DrawBox( pos.x, pos.y, Text, good )
 		end
 		
 	end
