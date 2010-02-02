e11d87c5e8f3e7ebef88a5d6c6849335469c9705
Kate Adams <self@kateadams.eu>
2010-02-02 19:59:24 +0000

    More damage delt
    git-svn-id: http://ambdevteam.googlecode.com/svn/trunk@101 1e2648ae-0935-11df-a26f-8b716316a5bc

--- AmBFightToSurvive/gamemode/prop_damage.lua
+++ AmBFightToSurvive/gamemode/prop_damage.lua
@@ -4,13 +4,13 @@ function f2sEntityTakeDamage( ent, inflictor, attacker, amount )
 		if ent.State then
 			state = ent:GetState()
 			if state == STATE_UNCONSTRUCTED || state == STATE_CONSTRUCTING || state == STATE_UNCONSTRUCTING then
-				ent.ResNeeded = ent.ResNeeded + ( amount / 20 )
+				ent.ResNeeded = ent.ResNeeded + ( amount / 10 )
 				if ent.ResNeeded - 10 > ent.Cost then
 					ent:Remove()				
 				end
 			end
 			if state == STATE_CONSTRUCTED  then
-				ent.ResNeeded = ent.ResNeeded + ( amount / 20 )
+				ent.ResNeeded = ent.ResNeeded + ( amount / 10 )
 				damage = ent.Cost - ent.ResNeeded
 				if damage < 0 then
 					ent:Remove()
