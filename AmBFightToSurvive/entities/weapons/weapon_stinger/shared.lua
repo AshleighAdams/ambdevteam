6ba851805bcdec50eb93f750596b680ea5b029aa
Kate Adams <self@kateadams.eu>
2010-02-07 21:10:09 +0000

    Updates to work ta home...
    git-svn-id: http://ambdevteam.googlecode.com/svn/trunk@138 1e2648ae-0935-11df-a26f-8b716316a5bc

--- AmBFightToSurvive/entities/weapons/weapon_stinger/shared.lua
+++ AmBFightToSurvive/entities/weapons/weapon_stinger/shared.lua
@@ -39,11 +39,11 @@ function SWEP:PrimaryAttack()
 	if CLIENT then return end
 	local ent = self.Owner:GetEyeTrace().Entity
 	if !ent && !ValidEntity(ent) then return end
-	self:FireMissile(nil, ent, 10, self.Owner )
+	self:FireMissile(nil, ent, 0.1, self.Owner )
 end
 
 function SWEP:FireMissile( pos, targent, aimspeed, pl )
-	local missile = ents.Create( "rpg_missile" )
+	local missile = ents.Create( "missile_rpg" )
 		missile:SetAngles( pl:EyeAngles() )
 		if pos then
 			missile:SetPos( pos )
@@ -52,8 +52,8 @@ function SWEP:FireMissile( pos, targent, aimspeed, pl )
 		end
 		missile:SetOwner(pl)
 		missile:Activate()
-		missile:Input("settimer",self.Owner,self.Owner,"4")
-		missile:SetVelocity(pl:GetAimVector()*700)
+		//missile:Input("settimer",self.Owner,self.Owner,"4")
+		//missile:SetVelocity(pl:GetAimVector()*700)
 	missile:Spawn()
 	local tname = "rpgaim+" .. tostring( CurTime() )
 	timer.Create( tname, 0.01, 0, function(as,m,t,tmr) -- aimspeed missile target timername
@@ -65,14 +65,16 @@ function SWEP:FireMissile( pos, targent, aimspeed, pl )
 		local mang = m:GetAngles()
 		local tang = (tpos - mpos):Angle() -- cur - targ to angle 
 		
-		local ang = Angle()
+		local ang = tang /*Angle()
 		
 		-- we want to limit the aim speed   --- mod 360 is for math errors, when the dir goes above 360 it does not loop around. now it does
-		ang.p = math.Clamp( tang.p, mang.p-as, mang.p+as ) //% 360
+		ang.p = math.Clamp( tang.p, mang.p-as, mang.p+as ) % 360
 		ang.y = math.Clamp( tang.y, mang.y-as, mang.y+as ) % 360
-		ang.r = math.Clamp( tang.r, mang.r-as, mang.r+as ) //% 360
+		ang.r = 0 //math.Clamp( tang.r, mang.r-as, mang.r+as ) //% 360 */
 		
+		local vel = m:GetVelocity()
 		m:SetAngles( ang )
+		m:GetPhysicsObject():SetVelocity( vel )
 		
 	end, aimspeed, missile, targent, tname)
 	return missile
