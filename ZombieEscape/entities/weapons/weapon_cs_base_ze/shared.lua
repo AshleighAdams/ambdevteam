434f20eb04ef09f7e9305132d1e52a44221c14da
Kate Adams <self@kateadams.eu>
2010-05-28 23:07:38 +0000

    Field Of View
    git-svn-id: http://ambdevteam.googlecode.com/svn/trunk@212 1e2648ae-0935-11df-a26f-8b716316a5bc

--- ZombieEscape/entities/weapons/weapon_cs_base_ze/shared.lua
+++ ZombieEscape/entities/weapons/weapon_cs_base_ze/shared.lua
@@ -27,7 +27,7 @@ if ( CLIENT ) then
 
 	SWEP.DrawAmmo			= true
 	SWEP.DrawCrosshair		= false
-	SWEP.ViewModelFOV		= 82
+	SWEP.ViewModelFOV		= 70
 	SWEP.ViewModelFlip		= true
 	SWEP.CSMuzzleFlashes	= true
 	
